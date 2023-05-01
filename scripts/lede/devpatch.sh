c9f6595c480912c4b5e2737e9316f9e76fb8485e
小木木 <d360493454@163.com>
2023-05-01 15:37:43 +0800

    切换插件克隆源

--- scripts/lede/devpatch.sh
+++ scripts/lede/devpatch.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#创建所需文件夹
-mkdir -p package/lean/r8152/patches
-mkdir -p target/linux/rockchip/files/drivers/phy/rockchip
-
-#更改默认地址为192.168.8.1
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-
-#更改r8152默认驱动
-sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/g' target/linux/rockchip/image/armv8.mk
-
-#添加内核配置
-sed -i '$a CONFIG_PHY_ROCKCHIP_INNO_USB3=y' target/linux/rockchip/armv8/config-5.15
-
 #删除feeds中自带的smartdns
 rm -rf ./feeds/packages/net/smartdns
 
