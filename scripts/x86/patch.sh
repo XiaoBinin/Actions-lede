5530db175bbab4e7f376a9df40db2cc07f27d237
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2023-06-04 18:24:13 +0800

    添加x86平台编译2

---
+++ scripts/x86/patch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#更改默认地址为192.168.8.1
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+#删除feeds中的插件
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/luci/applications/luci-app-mosdns
+
+#克隆插件
+rm -rf feeds/ssrp/ipt2socks
+
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/pwpage
+
+mkdir package/small
+pushd package/small
+#adguardhome
+git clone -b main https://github.com/XiaoBinin/luci-app-adguardhome.git
+#lucky
+git clone -b main https://github.com/sirpdboy/luci-app-lucky.git
+#smartdns
+git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
+git clone -b master https://github.com/pymumu/smartdns.git
+#ssrp
+#git clone -b master https://github.com/fw876/helloworld.git
+#passwall
+git clone -b luci --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+#passwall2
+git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git
+#mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+#openclash
+git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+
+popd
