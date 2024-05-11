d9a3f6d090008e87531ae8dc0ed3cbbb9859f923
XiaoBinin <d360493454@163.com>
2024-05-11 23:42:37 +0800

    update x86 patch.sh

--- scripts/x86/patch.sh
+++ scripts/x86/patch.sh
@@ -26,18 +26,18 @@ sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/
 mkdir package/small
 pushd package/small
 #adguardhome
-git clone -b main https://github.com/XiaoBinin/luci-app-adguardhome.git
+git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #lucky
-git clone -b main https://github.com/sirpdboy/luci-app-lucky.git
+git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 #smartdns
 git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
-git clone -b master https://github.com/pymumu/smartdns.git
+git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
 #ssrp
-git clone -b master https://github.com/fw876/helloworld.git
+git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 #passwall
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #passwall2
-git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 #mosdns
 git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 #openclash
