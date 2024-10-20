d86a69c343621eb62bc67624da9ed175d0dafcb5
XiaoBinin <d360493454@163.com>
2024-10-21 06:53:39 +0800

    scripts: 跟随上游源码刷新

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -4,5 +4,5 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 #更改design主题为白色
-sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
+# sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
 
--- scripts/packages.sh
+++ scripts/packages.sh
@@ -10,7 +10,7 @@
 # rm -rf ./feeds/ssrp/shadowsocksr-libev
 # rm -rf ./feeds/ssrp/v2ray-plugin
 # rm -rf ./feeds/packages/net/smartdns
-rm -rf ./feeds/luci/applications/luci-app-mosdns
+# rm -rf ./feeds/luci/applications/luci-app-mosdns
 # rm -rf ./feeds/luci/applications/luci-app-smartdns
 
 
@@ -21,22 +21,22 @@ rm -rf ./feeds/luci/applications/luci-app-mosdns
 mkdir package/small
 pushd package/small
 #adguardhome
-git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+# git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #lucky
-git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
+# git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 #smartdns
 # git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
 #ssrp
 git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 #passwall
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #passwall2
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 #mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+# git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 #openclash
-git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 
 popd
 
