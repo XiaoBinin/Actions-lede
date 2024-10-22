9637f94dc50a42fd29e1e3a9c95b7d276b9bc0f8
XiaoBinin <d360493454@163.com>
2024-10-23 06:40:03 +0800

    scripts: 跟随上游源码刷新
    修复lan默认地址问题

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -2,6 +2,7 @@
 
 #更改默认地址为192.168.8.1
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/luci2/bin/config_generate
 
 #更改design主题为白色
 # sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
