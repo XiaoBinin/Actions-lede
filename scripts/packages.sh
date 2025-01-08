6138ee9e9b37a24696e3fa3576ef94fdc6d5e405
XiaoBinin <d360493454@163.com>
2025-01-08 22:05:25 +0800

    scripts：使用上游smartdns，更改为默认不启动

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -9,29 +9,29 @@
 # rm -rf ./feeds/ssrp/naiveproxy
 # rm -rf ./feeds/ssrp/shadowsocksr-libev
 # rm -rf ./feeds/ssrp/v2ray-plugin
-# rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/chinadns-ng
 rm -rf ./feeds/packages/net/sing-box
 rm -rf ./feeds/packages/net/xray-core
 # rm -rf ./feeds/luci/applications/luci-app-mosdns
-# rm -rf ./feeds/luci/applications/luci-app-smartdns
+rm -rf ./feeds/luci/applications/luci-app-smartdns
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-passwall2
-
 
 #克隆插件
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
 
-
+#克隆的源码放在small文件夹
 mkdir package/small
 pushd package/small
+
 #adguardhome
 # git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #lucky
 # git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 #smartdns
-# git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
-# git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
+git clone -b master --depth 1 https://github.com/pymumu/luci-app-smartdns.git
+git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
 #ssrp
 # git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 #passwall
