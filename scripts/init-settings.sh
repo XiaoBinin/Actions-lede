4be97e0a5168d52ba64dcaebeef245aab6687575
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2025-05-25 15:32:56 +0800

    scripts: 给config下的文件增加权限

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -4,6 +4,9 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/luci2/bin/config_generate
 
+# 给config下的文件增加权限
+chmod 644 files/etc/config/*
+
 #更改design主题为白色
 # sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
 
