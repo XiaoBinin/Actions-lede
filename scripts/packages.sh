4f020a81b48c729e9bee2f09fc0fd6bbc7d22457
XiaoBinin <128011031+XiaoBinin@users.noreply.github.com>
2026-01-10 23:54:41 +0800

    configs: packages整理，增加homeproxy

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -26,23 +26,35 @@ git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git p
 
 mkdir package/small
 pushd package/small
+
 #adguardhome
 # git clone -b 2023.10 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+
+# homeproxy
+git clone -b master --depth 1 https://github.com/immortalwrt/homeproxy.git
+
 #lucky
 # git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
+
 #smartdns
 # git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
+
 #ssrp
 # git clone -b master --depth 1 https://github.com/fw876/helloworld.git
+
 #passwall
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+
 #passwall2
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+
 #mosdns
 # git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+
 #openclash
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+
 #modem
 # git clone -b main --depth 1 https://github.com/FUjr/modem_feeds.git
 
